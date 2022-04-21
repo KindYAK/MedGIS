56d8b1171dc851f0e7d01cf90c324701909301c4
KindYAK <yakunin.k@mail.ru>
2022-04-21 18:41:17 +0600

    feat - basic Hospital features

--- web/gisapp/models.py
+++ web/gisapp/models.py
@@ -50,7 +50,29 @@ class Hospital(GeoObject):
     town = models.ForeignKey('Town', on_delete=models.PROTECT, verbose_name="Населённый пункт")
     name = models.CharField(max_length=50, unique=True, verbose_name="Название")
 
-    # ...
+    address = models.CharField(max_length=250, verbose_name="Адрес")
+
+    number_of_doctors_with_category = models.PositiveSmallIntegerField(verbose_name="Количество врачей с категорией")
+    number_of_doctors_without_category = models.PositiveSmallIntegerField(verbose_name="Количество врачей без категории")
+    average_yearly_employees = models.FloatField(verbose_name="Среднегодовая численность работников")
+
+    area = models.FloatField(verbose_name="Площадь")
+    number_of_bed_places = models.PositiveSmallIntegerField(verbose_name="Количество койкомест")
+
+    clinical_rating_fb = models.FloatField(verbose_name="Клинический рейтинг ФБ")
+    clinical_rating_kr = models.FloatField(verbose_name="Клинический рейтинг КР")
+    clinical_rating_max = models.FloatField(verbose_name="Клинический рейтинг max")
+    clinical_rating_stars = models.FloatField(verbose_name="Клинический рейтинг - Звёзды")
+
+    management_rating_fb = models.FloatField(verbose_name="Рейтинг по показателям менеджмента ФБ")
+    management_rating_kr = models.FloatField(verbose_name="Рейтинг по показателям менеджмента КР")
+    management_rating_max = models.FloatField(verbose_name="Рейтинг по показателям менеджмента max")
+    management_rating_stars = models.FloatField(verbose_name="Рейтинг по показателям менеджмента - Звёзды")
+
+    number_of_patients_yearly = models.PositiveIntegerField(verbose_name="Количество пациентов за год")
+    male_patients_ratio = models.FloatField(verbose_name="Доля пациентов-мужчин")
+    female_patients_ratio = models.FloatField(verbose_name="Доля пациентов-женщин")
+    patients_average_stay_days = models.FloatField(verbose_name="Среднее время пребывания пациентов")
 
     class Meta:
         verbose_name = "Больница"
